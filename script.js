0bc59f0ae5797cc83b519aef242c2916fc2f1e7d
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-08-02 14:29:51 -0300

    terminando projeto

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ const calcular = document.getElementById("calcular")
 
 function imc() {
     /* puxando dados dentro da funçaõ */
-    const nome = document.getElementById("nome")
-    const altura = document.getElementById("altura")
-    const peso = document.getElementById("peso")
+    const nome = document.getElementById("nome").value
+    const altura = document.getElementById("altura").value
+    const peso = document.getElementById("peso").value
     const resultado = document.getElementById("resultado")
 
-    if (nome.value === '') {
-        alert('nome')
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(1) // to fixed arredonda o numero
+
+        let classificacao = ''
+
+        if (valorIMC < 18.5) {
+            classificacao = 'abaixo do peso ideal.'
+        } else if (valorIMC > 27.1) {
+            classificacao = 'com sobre peso.'
+        }  else {
+            classificacao ='com o peso ideal.'
+        }
+
+        resultado.innerText = `${nome}, seu IMC é de ${valorIMC}. E esta ${classificacao}`
+    } else {
+        resultado.innerText = 'Preencha todos os campos!'
     }
 
 }
